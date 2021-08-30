2ac708394c0ac3c9e129bf7f69328faf8c2318b7
fengxu <golangfxi@163.com>
2021-08-30 21:40:35 +0800

    update user namespace demo.

--- namespace/5.user/main.go
+++ namespace/5.user/main.go
@@ -15,6 +15,8 @@ func main() {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
 	}
 
+	cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
